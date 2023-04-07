b656a90d6beb8cff0d1dd021aeb78c5032ae9ac1
Wojtek <rossa21@gmail.com>
2023-04-07 16:25:08 +0200

    Created Headers layouts and styled them.

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,16 @@
+import {SearchBox} from "../../components/SearchBox";
+import {NavLink} from "react-router-dom";
+import "./Header.css";
+
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <NavLink to="/movies">Movies App</NavLink>
+                <NavLink to="/movies/login">Login</NavLink>
+                <NavLink to="/movies/register">Sign Up</NavLink>
+                <SearchBox/>
+            </nav>
+        </header>
+    );
+};
---
+++ src/layouts/LoggedHeader/LoggedHeader.tsx
@@ -0,0 +1,16 @@
+import {NavLink} from "react-router-dom";
+import {SearchBox} from "../../components/SearchBox";
+
+export const LoggedHeader = () => {
+    return (
+        <header>
+            <nav>
+                <NavLink to="/movies">Movies App</NavLink>
+                <NavLink to="#">My account</NavLink>
+                <NavLink to="/movies/login">Logout</NavLink>
+                <NavLink to="/movies/register">Delete</NavLink>
+                <SearchBox/>
+            </nav>
+        </header>
+    );
+};
