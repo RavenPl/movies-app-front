7aa1a21d3723caab5e996ce7bdbc180071d19700
Wojtek <rossa21@gmail.com>
2023-06-06 23:24:15 +0200

    Update version

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from "react-router-dom";
 
-import { RootLayout } from "./layouts/RootLayout";
-import { RegisterForm } from "./pages/RegisterForm/RegisterForm";
-import { GlobalProvider } from "./contexts/GlobalContext";
-import { LoginForm } from "./pages/LoginForm/LoginForm";
-import { LogoutUser } from "./pages/LogoutUser/LogoutUser";
-import { NotFoundView } from "./pages/NotFoundView/NotFoundView";
-import { DeleteUser } from "./pages/DeleteUser/DeleteUser";
-import { Bookmarks } from "./pages/Bookmarks/Bookmarks";
+import {RootLayout} from "./layouts/RootLayout";
+import {RegisterForm} from "./pages/RegisterForm/RegisterForm";
+import {GlobalProvider} from "./contexts/GlobalContext";
+import {LoginForm} from "./pages/LoginForm/LoginForm";
+import {LogoutUser} from "./pages/LogoutUser/LogoutUser";
+import {NotFoundView} from "./pages/NotFoundView/NotFoundView";
+import {DeleteUser} from "./pages/DeleteUser/DeleteUser";
+import {Bookmarks} from "./pages/Bookmarks/Bookmarks";
 import "./index.css";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/movies" element={<RootLayout />}>
-        <Route path="/movies/bookmarks" element={<Bookmarks />} />
-        <Route path="/movies/register" element={<RegisterForm />} />
-        <Route path="/movies/login" element={<LoginForm />} />
-        <Route path="/movies/logout" element={<LogoutUser />} />
-        <Route path="/movies/delete" element={<DeleteUser />} />
-      </Route>
-      <Route path="*" element={<NotFoundView />} />
-    </>
-  )
+    createRoutesFromElements(
+        <>
+            <Route path="/movies" element={<RootLayout/>}>
+                <Route path="/movies/bookmarks" element={<Bookmarks/>}/>
+                <Route path="/movies/register" element={<RegisterForm/>}/>
+                <Route path="/movies/login" element={<LoginForm/>}/>
+                <Route path="/movies/logout" element={<LogoutUser/>}/>
+                <Route path="/movies/delete" element={<DeleteUser/>}/>
+            </Route>
+            <Route path="*" element={<NotFoundView/>}/>
+        </>
+    )
 );
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <GlobalProvider>
-      <RouterProvider router={router} />
-    </GlobalProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalProvider>
+            <RouterProvider router={router}/>
+        </GlobalProvider>
+    </React.StrictMode>
 );
